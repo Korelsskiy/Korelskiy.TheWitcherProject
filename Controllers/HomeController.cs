54f4c72a4130e51f56f5d5849a29d96a502590b1
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-12-25 00:39:31 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Korelskiy.TheWitcherProject.Infrastructure;
+using Korelskiy.TheWitcherProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Korelskiy.TheWitcherProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext context;
+        public HomeController(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            IQueryable<WitcherItem> items = from i in context.ItemList orderby i.Id select i;
+
+            List<WitcherItem> WitcherItemList = await items.ToListAsync();
+
+            return View(WitcherItemList);
+        }
+    }
+}
